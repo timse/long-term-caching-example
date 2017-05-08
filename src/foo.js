981f6173052c51a94d286134d0a8cdb6811752a5
Tim Sebastian <tsebastian@atlassian.com>
2017-05-08 11:48:57 +1000

    additional async chunk breaks everything again

--- src/foo.js
+++ src/foo.js
@@ -1,5 +1,6 @@
 import bar from './bar';
 import preact from 'preact';
 import('./async-bar').then( a => console.log(a));
+import('./async-baz').then( a => console.log(a));
 
 console.log(preact.toString() + "hello world again and again");
