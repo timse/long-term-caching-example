6b710d06fdf40d4fdd921bb3fab3eb67926b387c
Tim Sebastian <tsebastian@atlassian.com>
2017-05-08 11:16:14 +1000

    add vendor chunk

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,15 @@ const webpack = require('webpack');
 module.exports = {
     entry: {
         main: './src/foo',
+        vendor: ['preact']
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].[hash].js',
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor']
+        })
+    ]
 }
